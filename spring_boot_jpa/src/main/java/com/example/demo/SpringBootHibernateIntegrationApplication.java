a0e9e9f264c53d1b1d4e2283d83f5dd8cb7b4af7
Tata Kowshik <tatakowshik@gmail.com>
2020-04-23 22:52:13 +0530

    jpa with swagger

--- spring_boot_jpa/src/main/java/com/example/demo/SpringBootHibernateIntegrationApplication.java
+++ spring_boot_jpa/src/main/java/com/example/demo/SpringBootHibernateIntegrationApplication.java
@@ -9,29 +9,26 @@ import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.data.Employee;
 
 @SpringBootApplication
-public class SpringBootHibernateIntegrationApplication implements CommandLineRunner {
+public class SpringBootHibernateIntegrationApplication  {
 
-	private EmployeeDAO employeeDAO;
-	
-	@Autowired
-	public SpringBootHibernateIntegrationApplication(EmployeeDAO employeeDAO) {
-		super();
-		this.employeeDAO = employeeDAO;
-	}
+	/*
+	 * private EmployeeDAO employeeDAO;
+	 * 
+	 * @Autowired public SpringBootHibernateIntegrationApplication(EmployeeDAO
+	 * employeeDAO) { super(); this.employeeDAO = employeeDAO; }
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHibernateIntegrationApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-	
-		Employee employee=new Employee("John","Doe","john@luv2code.com");
-		employeeDAO.createEmployee(employee);
-		employee=new Employee("Marry", "Joe", "marry@luv2code.com");
-		employeeDAO.createEmployee(employee);
-		employee=new Employee("Shane", "warne", "shane@luv2code.com");
-		employeeDAO.createEmployee(employee);
-	}
-
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * 
+	 * Employee employee=new Employee("John","Doe","john@luv2code.com");
+	 * employeeDAO.createEmployee(employee); employee=new Employee("Marry", "Joe",
+	 * "marry@luv2code.com"); employeeDAO.createEmployee(employee); employee=new
+	 * Employee("Shane", "warne", "shane@luv2code.com");
+	 * employeeDAO.createEmployee(employee); }
+	 */
 }
--- spring_boot_jpa/src/main/java/com/example/demo/rest/EmployeeController.java
+++ spring_boot_jpa/src/main/java/com/example/demo/rest/EmployeeController.java
@@ -21,9 +21,15 @@ import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.data.Employee;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
+@Api(value="Employee Management System", description="helps in Management of mployees ")
 public class EmployeeController {
 
 	private EmployeeDAO employeeDAO;
@@ -32,12 +38,20 @@ public class EmployeeController {
 		super();
 		this.employeeDAO = employeeDAO;
 	}
-	
+	@ApiOperation(value = "View a list of available employees in Managemnet System", response = List.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") 
+			})
 	@GetMapping("/employees")
 	public List<Employee> getAll()
 	{
 		return employeeDAO.getEmployees();
 	}
+	@ApiOperation(value = "View an particular employee in Managemnet System", response = List.class)
+	
 	@GetMapping("/employees/{id}")
 	public Employee getAll(@PathVariable("id") int id) throws EmployeeNotFoundException
 	{
@@ -49,11 +63,20 @@ public class EmployeeController {
 		}
 		return e;
 	}
+	@ApiOperation(value = "Creation of employee", response = List.class)
+	
 	@PostMapping("/employees")
 	public Employee create(@RequestBody Employee employee)
 	{
 		return employeeDAO.createEmployee(employee);
 	}
+	@ApiOperation(value = "Update Employees in Employee Managemnet System", response = List.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") 
+			})
 	@PutMapping("/employees")
 	public Employee update(@RequestBody Employee employee) throws EmployeeNotFoundException
 	{
@@ -64,6 +87,8 @@ public class EmployeeController {
 	 }
 	 return e;
 	}
+	@ApiOperation(value = "Delete  employees in Managemnet System", response = List.class)
+	
 	@DeleteMapping("/employees/{id}")
 	public Employee del(@PathVariable("id") int id) throws EmployeeNotFoundException
 	{
